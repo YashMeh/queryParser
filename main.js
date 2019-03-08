ea8f720a8c4b89b8483b7b124e5b7f0c9a420bce
YashMeh <mehrotra.yash9@gmail.com>
2019-03-08 23:27:10 +0530

    before refactor=>working

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ var server = require('http').createServer(app);
 var mongoose=require("mongoose");
 var port=process.env.PORT || 3000;
 var io = require('socket.io')(server);
+var registration=io.of('/register');
 var query=io.of('/query');
+var client=io.of('/client');
 var device=require("./models/device")
 //var global=[];
 mongoose.connect("mongodb://localhost/senzJS",{useNewUrlParser:true}).then((e)=>{
@@ -27,35 +29,63 @@ var alreadyPresent=(name)=>{
     })
 }
 var active=(name)=>{
-    device.find({name:name}).then((result)=>{
-        if(result[0].sessionID.length==0)
-        {
-            return false;
-        }
-        return true;
-    }).catch((err)=>{
-        console.log(err);
+    var obj={registered:false,active:false};
+    return new Promise((resolve,reject)=>{
+        device.find({name:name}).then((result)=>{
+            if(result.length==0)
+            {
+                resolve(obj);
+            }
+            else if(result[0].sessionID=="")
+            {
+                obj.registered=true;
+                resolve(obj);
+            }
+            else
+            {
+                obj.registered=true;
+                obj.active=true;
+                resolve(obj);
+            }
+        })
     })
 }
-var handleConnection=(socket)=>{
-    socket.emit("handshake",{message:"Client Connected",id:socket.id});
-    console.log("Connection request accepted from socket :"+socket.id);
+var assignID=(name,sessionID)=>{
+    return new Promise((resolve,reject)=>{
+        device.findOneAndUpdate({name:name},{$set:{sessionID:sessionID}}, {new: true}).then((foundDevice)=>{
+            resolve(foundDevice);
+        })
+    })
+}
+var sendData=(message,targetDevice)=>{
+    return new Promise((resolve,reject)=>{
+        device.find({name:targetDevice}).then((receiver)=>{
+            io.of("/client").to(receiver[0].sessionID).emit("received",{data:message});
+            resolve("Sent");
+        })
+    })
+}
+var handleRegistration=(socket)=>{
+    
+    console.log("Connection request accepted from socket :"+socket.id.substr(10,));
     socket.on("register",function(data){
         var senQuery=data.split(" ");
         if(senQuery[0]=='SHARE')
         {
             senQuery[6]=senQuery[6].substr(1,)
             alreadyPresent(senQuery[6]).then((present)=>{
+                
                 if(present)
             {
-                io.to(socket.id).emit("registered","Device name not available.");
+                
+                io.of('/register').to(socket.id).emit("registered",{registered:false});
                 //Disconnect the client
             }
             else
             {
                 var obj={name:senQuery[6],publicKey:senQuery[2]};
                 device.create(obj).then((createdDevice)=>{
-                    io.to(socket.id).emit("registered","Device registered.");
+                    io.of('/register').to(socket.id).emit("registered",{registered:true});
                     console.log(createdDevice);
                 }).catch((err)=>{
                     console.log(err);
@@ -73,19 +103,45 @@ var handleConnection=(socket)=>{
     })
 }
 var handleQuery=(socket)=>{
-    socket.emit("Connected to the query namespace");
-    //This will print /query#socket.id
     console.log("Connected to the query with socket :"+socket.id.substr(7,)); 
     socket.on("query",function(data){
         
         var senQuery=data.split(" ");
         if(senQuery[0]=='DATA')
         {
+            var targetDevice=senQuery[3].substr(1,)
+            active(targetDevice).then((result)=>{
+                if(result.registered==false)
+                {
+                    io.of('/query').to(socket.id).emit("sending","Target Device not registered");
+                }
+                else if(result.registered==true && result.active==false)
+                {
+                    io.of('/query').to(socket.id).emit("sending","Target Device not active");
+                }
+                else
+                {
+                    var message=senQuery[2];
+                    sendData(message,targetDevice).then((success)=>{
+                        io.of('/query').to(socket.id).emit('sending','Data sent');
+                    })
+                }
+            })
             
         }
     })
 }
-io.on("connection",handleConnection);
+var handleConnection=(socket)=>{
+    socket.on("giveID",function(name){
+        assignID(name,socket.id).then((assignedObj)=>{
+            io.of("/client").to(socket.id).emit("connected",{assigned:true});
+            console.log(assignedObj);
+        })
+
+    })
+}
+client.on("connection",handleConnection)
+registration.on("connection",handleRegistration);
 query.on("connection",handleQuery);
 server.listen(port,function(err){
     if(err)
